0c0aafed30c99d1874e95021767c33bcd5e37174
Melisa Altinyelek <melisaaltinyelek@hotmail.com>
2024-03-18 20:45:09 +0100

    added one-hot labels and comments

--- NN_model.py
+++ NN_model.py
@@ -3,26 +3,23 @@ from tensorflow import keras
 import numpy as np
 
 
-# # Define the LSTM Model
-# LSTM_model = tf.keras.Sequential([
-#     tf.keras.layers.Dense(input_num_units, activation = "relu"),
-#     tf.keras.layers.LSTM(hidden_num_units, activation = "tanh"),
-#     tf.keras.layers.Dense(output_num_units, activation = "softmax") 
-# ])
-
-
 class RNNModel(tf.keras.Model):
     def __init__(self, input_num_units, task_num_units, hidden_num_units, output_num_units):
         super(RNNModel, self).__init__()
+        # Define the input layer
         self.input_layer = tf.keras.layers.Dense(input_num_units, activation = "relu")
+        # Define the task layer
         self.task_layer = tf.keras.layers.Dense(task_num_units, activation = "relu")
+        # Define the recurrent connection by using LSTMCell
         lstm_cell = tf.keras.layers.LSTMCell(units = hidden_num_units)
+        # Pass the LSTMCell to the hidden RNN layer to achieve a recurrent connection
         self.hidden_layer = tf.keras.layers.RNN(lstm_cell, activation = "tanh", return_sequences = True)
-        self.output_layer = tf.keras.layers.Dense(output_num_units, activation = "linear")
+        # Define the output layer
+        self.output_layer = tf.keras.layers.Dense(output_num_units, activation = "softmax")
 
-    def call(self, inputs):
-        x = self.input_layer(inputs)
-        task_output = self.task_layer(inputs)
+    def call(self, input, task):
+        x = self.input_layer(input)
+        task_output = self.task_layer(task)
         x = tf.concat([x, task_output], axis = 1)
         x = self.hidden_layer(x)
         output = self.output_layer(x)
@@ -36,6 +33,13 @@ hidden_num_units = 100
 output_num_units = 9
 
 
+
+
 # Create an instance of the RNNModel
 LSTM_model = RNNModel(input_num_units, task_num_units, hidden_num_units, output_num_units)
 
+# Creating one-hot encoded labels for the input, task and output layers
+# "None" will be replaced by the actual data
+input_layer_one_hot = tf.one_hot(None, depth = input_num_units)
+task_layer_one_hot = tf.one_hot(None, depth = task_num_units)
+output_layer_one_hot = tf.one_hot(None, depth = output_num_units)
