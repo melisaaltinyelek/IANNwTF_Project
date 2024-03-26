9bfd845468c9a4c0e1a64685cb4626adba17dc85
Jenny Arndt <77383916+jearndt@users.noreply.github.com>
2024-03-26 17:03:38 +0100

    Update LSTM_example.py
    - allows input of our desired shape
    - Added "plot results"

--- LSTM_example.py
+++ LSTM_example.py
@@ -5,21 +5,22 @@ from pandas import concat
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from matplotlib import pyplot as plt
 # create sequence
-length = 10
+length = 270*20*15 + 1
 sequence = [i/float(length) for i in range(length)]
-# create X/y pairs
+# create X/y pairs; X : input, y:output
 df = DataFrame(sequence)
 df = concat([df, df.shift(1)], axis=1)
 df.dropna(inplace=True)
 # convert to LSTM friendly format
 values = df.values
 X, y = values[:, 0], values[:, 1]
-X = X.reshape(3, 3, 1)
-y = y.reshape(3,3,1)
+X = X.reshape(270,20,15)
+y = y.reshape(270,20,15)
 # configure network
 n_batch = 1
-n_epoch = 100
+n_epoch = 22
 n_neurons = 10
 # design network
 model = Sequential()
@@ -28,8 +29,12 @@ model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 # fit network
 #%%
-for i in range(n_epoch):
- model.fit(X, y, epochs=1, batch_size=n_batch, verbose=1, shuffle=False)
- model.reset_states()
+
+history = model.fit(X, y, epochs=n_epoch, batch_size=n_batch, verbose=1, shuffle=False)
+
+# %%
+# plot results
+plt.plot(history.history['loss'])
+plt.legend()
 
 # %%
