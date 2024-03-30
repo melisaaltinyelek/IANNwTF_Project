75bc176a3f5c881c05d790d8162ac9aff68b4ad6
Jenny Arndt <77383916+jearndt@users.noreply.github.com>
2024-03-30 14:17:08 +0100

    Update LSTM_ourdata_MSE.py
    Now possible to test the same network on both factors:
    (1) CONDITION; 2 levels: A&B and A&C
    (2) CUE POSTITION; 10 levels: 0,1,2,3,4,5,6,7,8,9

--- LSTM_ourdata_MSE.py
+++ LSTM_ourdata_MSE.py
@@ -1,4 +1,4 @@
-# Latest Update: 29.03.2024
+# Latest Update: 30.03.2024
 #%%
 import pandas as pd
 from pandas import DataFrame, concat
@@ -25,7 +25,7 @@ train_ds_pred = [literal_eval(x) for x in df["curr_output"].tolist()]
 
 # test_ds = [literal_eval(x) for x in df_val["input"].tolist()]
 # test_ds_pred = [literal_eval(x) for x in df_val["curr_output"].tolist()]
-#%%
+
 def create_test_samples(df_val, cue_position = 0, condition = "B"):
     if cue_position > 9:
         print(f"cue_position is greater than 9! Given is: {cue_position}")
@@ -47,63 +47,116 @@ test_ds, test_ds_pred = create_test_samples(df_val = df_val, cue_position = 9, c
 # Check the shape of the training dataset:
 # np.shape(train_ds) -> (X,20,15)
 # np.shape(train_ds_pred) -> (X,9)
-df_all_accuracies = pd.DataFrame()
+df_AB_all_accuracies = pd.DataFrame()
+df_AC_all_accuracies = pd.DataFrame()
 
 # callback
 # https://www.tensorflow.org/api_docs/python/tf/keras/Model#evaluate
 # https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/Callback  & https://stackoverflow.com/questions/47731935/using-multiple-validation-sets-with-keras
-df_accuracy_cue_pos = pd.DataFrame()
+dfAB_accuracy_cue_pos = pd.DataFrame()
+dfAC_accuracy_cue_pos = pd.DataFrame()
 class MyCustomCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None, df_val = df_val, condition = "C"):
-        global df_accuracy_cue_pos
-        X_test_0, y_test_0 = create_test_samples(df_val, cue_position = 0, condition = condition)
-        X_test_1, y_test_1 = create_test_samples(df_val, cue_position = 1, condition = condition)
-        X_test_2, y_test_2 = create_test_samples(df_val, cue_position = 2, condition = condition)
-        X_test_3, y_test_3 = create_test_samples(df_val, cue_position = 3, condition = condition)
-        X_test_4, y_test_4 = create_test_samples(df_val, cue_position = 4, condition = condition)
-        X_test_5, y_test_5 = create_test_samples(df_val, cue_position = 5, condition = condition)
-        X_test_6, y_test_6 = create_test_samples(df_val, cue_position = 6, condition = condition)
-        X_test_7, y_test_7 = create_test_samples(df_val, cue_position = 7, condition = condition)
-        X_test_8, y_test_8 = create_test_samples(df_val, cue_position = 8, condition = condition)
-        X_test_9, y_test_9 = create_test_samples(df_val, cue_position = 9, condition = condition)
+    def on_epoch_end(self, epoch, logs=None, df_val = df_val):
+        # for condition A&B
+        global dfAB_accuracy_cue_pos
+        X_AB_test_0, y_AB_test_0 = create_test_samples(df_val, cue_position = 0, condition = "B")
+        X_AB_test_1, y_AB_test_1 = create_test_samples(df_val, cue_position = 1, condition = "B")
+        X_AB_test_2, y_AB_test_2 = create_test_samples(df_val, cue_position = 2, condition = "B")
+        X_AB_test_3, y_AB_test_3 = create_test_samples(df_val, cue_position = 3, condition = "B")
+        X_AB_test_4, y_AB_test_4 = create_test_samples(df_val, cue_position = 4, condition = "B")
+        X_AB_test_5, y_AB_test_5 = create_test_samples(df_val, cue_position = 5, condition = "B")
+        X_AB_test_6, y_AB_test_6 = create_test_samples(df_val, cue_position = 6, condition = "B")
+        X_AB_test_7, y_AB_test_7 = create_test_samples(df_val, cue_position = 7, condition = "B")
+        X_AB_test_8, y_AB_test_8 = create_test_samples(df_val, cue_position = 8, condition = "B")
+        X_AB_test_9, y_AB_test_9 = create_test_samples(df_val, cue_position = 9, condition = "B")
 
-        res_eval_0 = self.model.evaluate(np.array(X_test_0), np.array(y_test_0), verbose = 0)
-        res_eval_1 = self.model.evaluate(np.array(X_test_1), np.array(y_test_1), verbose = 0)
-        res_eval_2 = self.model.evaluate(np.array(X_test_2), np.array(y_test_2), verbose = 0)
-        res_eval_3 = self.model.evaluate(np.array(X_test_3), np.array(y_test_3), verbose = 0)
-        res_eval_4 = self.model.evaluate(np.array(X_test_4), np.array(y_test_4), verbose = 0)
-        res_eval_5 = self.model.evaluate(np.array(X_test_5), np.array(y_test_5), verbose = 0)
-        res_eval_6 = self.model.evaluate(np.array(X_test_6), np.array(y_test_6), verbose = 0)
-        res_eval_7 = self.model.evaluate(np.array(X_test_7), np.array(y_test_7), verbose = 0)
-        res_eval_8 = self.model.evaluate(np.array(X_test_8), np.array(y_test_8), verbose = 0)
-        res_eval_9 = self.model.evaluate(np.array(X_test_9), np.array(y_test_9), verbose = 0)
+        res_AB_eval_0 = self.model.evaluate(np.array(X_AB_test_0), np.array(y_AB_test_0), verbose = 0)
+        res_AB_eval_1 = self.model.evaluate(np.array(X_AB_test_1), np.array(y_AB_test_1), verbose = 0)
+        res_AB_eval_2 = self.model.evaluate(np.array(X_AB_test_2), np.array(y_AB_test_2), verbose = 0)
+        res_AB_eval_3 = self.model.evaluate(np.array(X_AB_test_3), np.array(y_AB_test_3), verbose = 0)
+        res_AB_eval_4 = self.model.evaluate(np.array(X_AB_test_4), np.array(y_AB_test_4), verbose = 0)
+        res_AB_eval_5 = self.model.evaluate(np.array(X_AB_test_5), np.array(y_AB_test_5), verbose = 0)
+        res_AB_eval_6 = self.model.evaluate(np.array(X_AB_test_6), np.array(y_AB_test_6), verbose = 0)
+        res_AB_eval_7 = self.model.evaluate(np.array(X_AB_test_7), np.array(y_AB_test_7), verbose = 0)
+        res_AB_eval_8 = self.model.evaluate(np.array(X_AB_test_8), np.array(y_AB_test_8), verbose = 0)
+        res_AB_eval_9 = self.model.evaluate(np.array(X_AB_test_9), np.array(y_AB_test_9), verbose = 0)
 
-        if (df_accuracy_cue_pos.empty):
-            # store accuracy in df_accuracy_cue_pos
-            df_accuracy_cue_pos.insert(0, "val_acc_cuepos0", [res_eval_0[1]], True)
-            df_accuracy_cue_pos.insert(1, "val_acc_cuepos1", [res_eval_1[1]], True)
-            df_accuracy_cue_pos.insert(2, "val_acc_cuepos2", [res_eval_2[1]], True)
-            df_accuracy_cue_pos.insert(3, "val_acc_cuepos3", [res_eval_3[1]], True)
-            df_accuracy_cue_pos.insert(4, "val_acc_cuepos4", [res_eval_4[1]], True)
-            df_accuracy_cue_pos.insert(5, "val_acc_cuepos5", [res_eval_5[1]], True)
-            df_accuracy_cue_pos.insert(6, "val_acc_cuepos6", [res_eval_6[1]], True)
-            df_accuracy_cue_pos.insert(7, "val_acc_cuepos7", [res_eval_7[1]], True)
-            df_accuracy_cue_pos.insert(8, "val_acc_cuepos8", [res_eval_8[1]], True)
-            df_accuracy_cue_pos.insert(9, "val_acc_cuepos9", [res_eval_9[1]], True)
+        if (dfAB_accuracy_cue_pos.empty):
+            # store accuracy in dfAB_accuracy_cue_pos
+            dfAB_accuracy_cue_pos.insert(0, "val_acc_cuepos0", [res_AB_eval_0[1]], True)
+            dfAB_accuracy_cue_pos.insert(1, "val_acc_cuepos1", [res_AB_eval_1[1]], True)
+            dfAB_accuracy_cue_pos.insert(2, "val_acc_cuepos2", [res_AB_eval_2[1]], True)
+            dfAB_accuracy_cue_pos.insert(3, "val_acc_cuepos3", [res_AB_eval_3[1]], True)
+            dfAB_accuracy_cue_pos.insert(4, "val_acc_cuepos4", [res_AB_eval_4[1]], True)
+            dfAB_accuracy_cue_pos.insert(5, "val_acc_cuepos5", [res_AB_eval_5[1]], True)
+            dfAB_accuracy_cue_pos.insert(6, "val_acc_cuepos6", [res_AB_eval_6[1]], True)
+            dfAB_accuracy_cue_pos.insert(7, "val_acc_cuepos7", [res_AB_eval_7[1]], True)
+            dfAB_accuracy_cue_pos.insert(8, "val_acc_cuepos8", [res_AB_eval_8[1]], True)
+            dfAB_accuracy_cue_pos.insert(9, "val_acc_cuepos9", [res_AB_eval_9[1]], True)
         else:
-            df_temp_accuracy_cue_pos = pd.DataFrame()
-            df_temp_accuracy_cue_pos.insert(0, "val_acc_cuepos0", [res_eval_0[1]], True)
-            df_temp_accuracy_cue_pos.insert(1, "val_acc_cuepos1", [res_eval_1[1]], True)
-            df_temp_accuracy_cue_pos.insert(2, "val_acc_cuepos2", [res_eval_2[1]], True)
-            df_temp_accuracy_cue_pos.insert(3, "val_acc_cuepos3", [res_eval_3[1]], True)
-            df_temp_accuracy_cue_pos.insert(4, "val_acc_cuepos4", [res_eval_4[1]], True)
-            df_temp_accuracy_cue_pos.insert(5, "val_acc_cuepos5", [res_eval_5[1]], True)
-            df_temp_accuracy_cue_pos.insert(6, "val_acc_cuepos6", [res_eval_6[1]], True)
-            df_temp_accuracy_cue_pos.insert(7, "val_acc_cuepos7", [res_eval_7[1]], True)
-            df_temp_accuracy_cue_pos.insert(8, "val_acc_cuepos8", [res_eval_8[1]], True)
-            df_temp_accuracy_cue_pos.insert(9, "val_acc_cuepos9", [res_eval_9[1]], True)   
-            df_accuracy_cue_pos = pd.concat([df_accuracy_cue_pos, df_temp_accuracy_cue_pos])  
+            dfAB_temp_accuracy_cue_pos = pd.DataFrame()
+            dfAB_temp_accuracy_cue_pos.insert(0, "val_acc_cuepos0", [res_AB_eval_0[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(1, "val_acc_cuepos1", [res_AB_eval_1[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(2, "val_acc_cuepos2", [res_AB_eval_2[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(3, "val_acc_cuepos3", [res_AB_eval_3[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(4, "val_acc_cuepos4", [res_AB_eval_4[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(5, "val_acc_cuepos5", [res_AB_eval_5[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(6, "val_acc_cuepos6", [res_AB_eval_6[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(7, "val_acc_cuepos7", [res_AB_eval_7[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(8, "val_acc_cuepos8", [res_AB_eval_8[1]], True)
+            dfAB_temp_accuracy_cue_pos.insert(9, "val_acc_cuepos9", [res_AB_eval_9[1]], True)   
+            dfAB_accuracy_cue_pos = pd.concat([dfAB_accuracy_cue_pos, dfAB_temp_accuracy_cue_pos])  
+        
+        # for condition A&C
+        global dfAC_accuracy_cue_pos
+        X_AC_test_0, y_AC_test_0 = create_test_samples(df_val, cue_position = 0, condition = "C")
+        X_AC_test_1, y_AC_test_1 = create_test_samples(df_val, cue_position = 1, condition = "C")
+        X_AC_test_2, y_AC_test_2 = create_test_samples(df_val, cue_position = 2, condition = "C")
+        X_AC_test_3, y_AC_test_3 = create_test_samples(df_val, cue_position = 3, condition = "C")
+        X_AC_test_4, y_AC_test_4 = create_test_samples(df_val, cue_position = 4, condition = "C")
+        X_AC_test_5, y_AC_test_5 = create_test_samples(df_val, cue_position = 5, condition = "C")
+        X_AC_test_6, y_AC_test_6 = create_test_samples(df_val, cue_position = 6, condition = "C")
+        X_AC_test_7, y_AC_test_7 = create_test_samples(df_val, cue_position = 7, condition = "C")
+        X_AC_test_8, y_AC_test_8 = create_test_samples(df_val, cue_position = 8, condition = "C")
+        X_AC_test_9, y_AC_test_9 = create_test_samples(df_val, cue_position = 9, condition = "C")
 
+        res_AC_eval_0 = self.model.evaluate(np.array(X_AC_test_0), np.array(y_AC_test_0), verbose = 0)
+        res_AC_eval_1 = self.model.evaluate(np.array(X_AC_test_1), np.array(y_AC_test_1), verbose = 0)
+        res_AC_eval_2 = self.model.evaluate(np.array(X_AC_test_2), np.array(y_AC_test_2), verbose = 0)
+        res_AC_eval_3 = self.model.evaluate(np.array(X_AC_test_3), np.array(y_AC_test_3), verbose = 0)
+        res_AC_eval_4 = self.model.evaluate(np.array(X_AC_test_4), np.array(y_AC_test_4), verbose = 0)
+        res_AC_eval_5 = self.model.evaluate(np.array(X_AC_test_5), np.array(y_AC_test_5), verbose = 0)
+        res_AC_eval_6 = self.model.evaluate(np.array(X_AC_test_6), np.array(y_AC_test_6), verbose = 0)
+        res_AC_eval_7 = self.model.evaluate(np.array(X_AC_test_7), np.array(y_AC_test_7), verbose = 0)
+        res_AC_eval_8 = self.model.evaluate(np.array(X_AC_test_8), np.array(y_AC_test_8), verbose = 0)
+        res_AC_eval_9 = self.model.evaluate(np.array(X_AC_test_9), np.array(y_AC_test_9), verbose = 0)
+
+        if (dfAC_accuracy_cue_pos.empty):
+            # store accuracy in dfAB_accuracy_cue_pos
+            dfAC_accuracy_cue_pos.insert(0, "val_acc_cuepos0", [res_AC_eval_0[1]], True)
+            dfAC_accuracy_cue_pos.insert(1, "val_acc_cuepos1", [res_AC_eval_1[1]], True)
+            dfAC_accuracy_cue_pos.insert(2, "val_acc_cuepos2", [res_AC_eval_2[1]], True)
+            dfAC_accuracy_cue_pos.insert(3, "val_acc_cuepos3", [res_AC_eval_3[1]], True)
+            dfAC_accuracy_cue_pos.insert(4, "val_acc_cuepos4", [res_AC_eval_4[1]], True)
+            dfAC_accuracy_cue_pos.insert(5, "val_acc_cuepos5", [res_AC_eval_5[1]], True)
+            dfAC_accuracy_cue_pos.insert(6, "val_acc_cuepos6", [res_AC_eval_6[1]], True)
+            dfAC_accuracy_cue_pos.insert(7, "val_acc_cuepos7", [res_AC_eval_7[1]], True)
+            dfAC_accuracy_cue_pos.insert(8, "val_acc_cuepos8", [res_AC_eval_8[1]], True)
+            dfAC_accuracy_cue_pos.insert(9, "val_acc_cuepos9", [res_AC_eval_9[1]], True)
+        else:
+            dfAC_temp_accuracy_cue_pos = pd.DataFrame()
+            dfAC_temp_accuracy_cue_pos.insert(0, "val_acc_cuepos0", [res_AC_eval_0[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(1, "val_acc_cuepos1", [res_AC_eval_1[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(2, "val_acc_cuepos2", [res_AC_eval_2[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(3, "val_acc_cuepos3", [res_AC_eval_3[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(4, "val_acc_cuepos4", [res_AC_eval_4[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(5, "val_acc_cuepos5", [res_AC_eval_5[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(6, "val_acc_cuepos6", [res_AC_eval_6[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(7, "val_acc_cuepos7", [res_AC_eval_7[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(8, "val_acc_cuepos8", [res_AC_eval_8[1]], True)
+            dfAC_temp_accuracy_cue_pos.insert(9, "val_acc_cuepos9", [res_AC_eval_9[1]], True)   
+            dfAC_accuracy_cue_pos = pd.concat([dfAC_accuracy_cue_pos, dfAC_temp_accuracy_cue_pos])  
+        
 # plot losses and accuracies
 def flatten_list(l):
     l_flattend =  [x for xs in l for x in xs] 
@@ -115,6 +168,9 @@ def convert_df(df):
     df = df.rename(columns={0:col_names[0], 1: col_names[1]})
     return df
 #%%
+helper_index = 0 # only set if you run this cell for the first time
+helper_index += 1
+
 X = np.array(train_ds)
 y = np.array(train_ds_pred)
 
@@ -126,7 +182,8 @@ n_batch = 1
 n_epoch = 20
 n_neurons = 5 #100 keep it as low as possible - such that the network is capable to solve the task (and is stable for same hyperparameters) (if it is to large, the network too complex the task is too easy and the few epochs needed until desired_mse)
 my_val_callback = MyCustomCallback()
-df_accuracy_cue_pos = pd.DataFrame()
+dfAB_accuracy_cue_pos = pd.DataFrame()
+dfAC_accuracy_cue_pos = pd.DataFrame()
 
 # Initialize the network and add an LSTM layer
 model = Sequential()
@@ -159,14 +216,29 @@ while True:
         break
 
 # store all results in df
-df_all_accuracies = pd.concat([df_all_accuracies, df_accuracy_cue_pos.reset_index()]) # not reset_index()
+df_AB_all_accuracies = pd.concat([df_AB_all_accuracies, dfAB_accuracy_cue_pos.reset_index()])
+df_AC_all_accuracies = pd.concat([df_AC_all_accuracies, dfAC_accuracy_cue_pos.reset_index()])
 
 # plot results
 loss_df = pd.DataFrame.from_dict(mse_history)
-df_accuracy_cue_pos.reset_index().drop("index", axis = 1).plot()
+
+in_title = "model_" + str(helper_index)
+
+print("\n CONDITION A&B: \n")
+dfAB_accuracy_cue_pos.reset_index().drop("index", axis = 1).plot()
 convert_df(loss_df)["accuracy"].plot()
+title_AC = "Condition A&B: " + in_title
+plt.title(title_AC)
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-df_accuracy_cue_pos
+print(dfAB_accuracy_cue_pos)
+
+print("\n CONDITION A&C: \n")
+dfAC_accuracy_cue_pos.reset_index().drop("index", axis = 1).plot()
+convert_df(loss_df)["accuracy"].plot()
+title_AC = "Condition A&C: " + in_title 
+plt.title(title_AC)
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+print(dfAB_accuracy_cue_pos)
 #%%
 # Plot the MSE history across epochs
 plt.plot(mse_history)
@@ -174,5 +246,4 @@ plt.xlabel("Epoch")
 plt.ylabel("Mean Squared Error")
 plt.title("Training MSE History")
 plt.show()
-
 
